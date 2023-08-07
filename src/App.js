3bac44a547f54f468e96ad4ac90306eec32d7c58
mayanksrivastava1 <100144534+mayanksrivastava1@users.noreply.github.com>
2023-08-07 22:39:12 +0530

    Assignment-Movie List

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ShowList from './Components/ShowsList';
@@ -6,9 +5,9 @@ import ShowSummary from './Components/ShowSummary';
 
 const App = () => {
   return (
-    <Routes> {/* Use Routes instead of Switch */}
-        <Route path="/" element={<ShowList />} /> {/* Use "element" instead of "component" */}
-        <Route path="/show/:id" element={<ShowSummary />} /> {/* Use "element" instead of "component" */}
+    <Routes> 
+        <Route path="/" element={<ShowList />} /> 
+        <Route path="/show/:id" element={<ShowSummary />} /> 
     </Routes>
 
   );
